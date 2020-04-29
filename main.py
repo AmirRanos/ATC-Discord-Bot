ceaa1cc2cee3848f9a030186998509cd035c2f0f
Naftoreiclag <naftoreiclag@gmail.com>
2020-04-28 23:13:19 -0700

    Add basic bot code, proof-of-concept for TTS

---
+++ main.py
@@ -0,0 +1,70 @@
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+	print('Logged in as {}'.format(client.user))
+
+def sanitize(string):
+	retval = []
+	for c in string:
+		if c.lower() in 'abcdefghijklmnopqrstuvwxyz0123456789 ,.?!':
+			retval.append(c)
+	return ''.join(retval)
+
+def make_voice_pico(output_fname, msg):
+	command = 'pico2wave -w {} "{}"'.format(output_fname, sanitize(msg))
+	os.system(command)
+	
+def make_voice_festival(output_fname, msg):
+	command = 'echo "{}" | text2wave -eval "(voice_cmu_us_slt_arctic_hts)" -o {}'.format(sanitize(msg), output_fname)
+	os.system(command)
+
+@client.event
+async def on_message(message):
+	if message.author.bot:
+		return
+		
+	if message.content.startswith('`atc'):
+		await join_voice_channel(client, message.author.voice.channel)
+
+async def join_voice_channel(client, voice_channel):
+	voice_client = voice_channel.guild.voice_client
+	if voice_client is None or voice_client.channel != voice_channel:
+		try:
+			voice_client = await voice_channel.connect()
+		except discord.client.ClientException as e:
+			pass
+	return voice_client
+
+@client.event
+async def on_voice_state_update(member, before, after):
+	print(member, before.channel, after.channel)
+	
+	announce = None
+	
+	bot_client = member.guild.voice_client
+	if bot_client is not None:
+		if after.channel != before.channel:
+			if after.channel == bot_client.channel:
+				announce = 'join'
+			elif before.channel == bot_client.channel:
+				announce = 'leave'
+			
+	if announce is not None:
+		message = ''
+		display_name = member.display_name
+		if member.bot:
+			display_name = 'service droid'
+		if announce == 'join':
+			message = 'Welcome {}.'.format(display_name)
+		elif announce == 'leave':
+			message = 'Goodbye {}.'.format(display_name)
+		
+		ofname = 'voice.wav'
+		make_voice_festival(ofname, message)
+		bot_client.play(discord.FFmpegOpusAudio(ofname))
+
+client.run('TOKEN HERE')
